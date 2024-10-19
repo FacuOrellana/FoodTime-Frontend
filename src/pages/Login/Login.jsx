b2633db160042a83635ff9a0eba8499a56f42ab5
orellanagonzalo64 <orellanagonzalo64@gmail.com>
2024-10-19 17:01:57 -0300

    Boton registrarse

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -90,6 +90,13 @@ const LoginForm = () => {
 
               <button
                 className="ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                href="/password/request"
+              >
+                Registrarse
+              </button>
+
+              <button
+                className="ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
                 type="submit"
               >
                 Iniciar sesión
